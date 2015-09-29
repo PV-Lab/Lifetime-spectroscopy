f5d68a35dcd5a231cc11ef4b36133e38f0d5cf66
maljensen <jensenma@mit.edu>
2015-09-29 10:23:38 -0400

    Adding new functions and ignore .asv files

---
+++ average_tau.m
@@ -0,0 +1,52 @@
+function [tau_mean,deltanq]=average_tau(filename)
+%average_tau(filename) processes a .mat file which has been produced by
+%process_xls_data. All of the data sets within the file are assumed to
+%belong the same sample. The curves are interpolated and then averaged to
+%obtain an average curve.
+
+load(filename); 
+
+deltanq = logspace(13,17,500); %typical max range for the QSSPC tool
+
+together = figure; 
+
+for i = 1:length(fileListShort)
+    data = dataSave{i};
+    deltan = data(:,1); 
+    tau = data(:,2);
+    
+    %We need to check and make sure no deltan (x-values) are repeated. This
+    %will interfere with the interpolation. 
+    deltan = flipud(deltan);
+    tau = flipud(tau); 
+    [deltan,IX] = sort(deltan); 
+    tau = tau(IX); 
+    store_index = []; 
+    count = 1; 
+    for j = 1:length(deltan)
+        index = find(deltan==deltan(j)); 
+        [m,n] = size(index); 
+        if m>1
+            store_index(count:count+m-2) = index(2:end); 
+            count = count+1; 
+        end
+    end
+    deltan(store_index) = [];
+    tau(store_index) = [];
+    
+    %Plot the resulting curve
+    loglog(deltan,tau.*1e6,'.','MarkerSize',5);
+    hold all;
+    
+    %Interpolate at the query points
+    tauq(:,i) = interp1(deltan,tau,deltanq);
+end
+
+%Find the mean of the lifetime
+tau_mean = nanmean(tauq,2); 
+
+%Plot the result
+loglog(deltanq,tau_mean.*1e6,'-','LineWidth',2); 
+
+xlabel('Excess carrier density (cm^-^3)','FontSize',30);
+ylabel('Lifetime (\mus)','FontSize',30);
---
+++ compare_QSSPC_lifetime.m
@@ -0,0 +1,99 @@
+function compare_QSSPC_lifetime(deltanQSSPC,tauQSSPC,deltanLifetime,tauLifetime,filenameSintonCircle)
+%compare_QSSPC_lifetime(deltanQSSPC,tauQSSPC,deltanLifetime,tauLifetime,filenameSintonCircle).
+%This function takes as input a single QSSPC curve and a single pair of
+%spatially resolved lifetime/injection level maps. The additional input is
+%the filename for the Sinton circle - this script currently assumes the
+%maps are generated from PC-PL. The units of the lifetime should be in
+%microseconds in both cases. A figure is generated comparing the
+%injection-dependent lifetime curve the both the arithmetic and harmonic
+%means around the measured area. 
+
+
+%We need to get the spatially-resolved lifetime averages for comparison
+
+%If PC-PL, use the image of the Sinton circle to choose the averaging area
+figure; 
+%Define delimiter and header in order to read txt files
+delimiterIn = ',';
+headerlinesIn = 0;
+PLsensor = importdata(filenameSintonCircle,delimiterIn,headerlinesIn);
+figure;
+imagesc(PLsensor);
+axis('image');
+
+circletest = 0;
+while circletest == 0
+    
+    PLsensor_circletest = PLsensor;
+    
+    disp('Click the center of the circle ...');
+    [x,y] = ginput(1);
+    disp('Click the edge of the circle ...');
+    [xr,yr] = ginput(1);
+    radius = sqrt((x-xr)^2 + (y-yr)^2);
+    
+    %PL maps have 1024 x 1024 pixels
+    %set all pixels outside of the sensor region to NaN
+    for i = 1:1024
+        for j = 1:1024
+            if abs(sqrt(abs(i-y)^2+abs(j-x)^2))>radius
+                PLsensor_circletest(i,j) = NaN;
+            end
+        end
+    end
+    
+    %Plot the new map with the sensor region only
+    figure;
+    imagesc(PLsensor_circletest);
+    axis('image');
+    
+    promptcorrect = 'Is the mask acceptable?';
+    str = input(promptcorrect,'s');
+    if isempty(str)
+        str = 'Y';
+    end
+    
+    if str == 'Y'
+        circletest = 1;
+    end
+    
+end
+
+%Mask the spatially-resolved lifetime and injection maps
+[m,n] = size(deltanLifetime); 
+for i = 1:m
+    for j = 1:n
+        if abs(sqrt(abs(i-y)^2+abs(j-x)^2))>radius
+            deltanLifetime(i,j) = NaN;
+            tauLifetime(i,j) = NaN;
+        end
+    end
+end
+
+%Plot the results 
+figure;
+imagesc(deltanLifetime); 
+axis('image');
+title('Masked injection level');
+
+figure;
+imagesc(tauLifetime);
+axis('image');
+title('Masked lifetime'); 
+
+[deltanLinear,deltan_Mp] = tau_averages(deltanLifetime);
+[tauLinear,tau_Mp] = tau_averages(tauLifetime);
+
+figure;
+h(1)=loglog(deltanQSSPC,tauQSSPC,'LineWidth',2); 
+hold all;
+h(2) = loglog(deltanLinear,tauLinear,'o','MarkerSize',8);
+hold all;
+h(3) = loglog(deltan_Mp,tau_Mp,'s','MarkerSize',8);
+
+xlabel('Excess carrier density (cm^-^3)','FontSize',30);
+ylabel('Lifetime (\mus)','FontSize',30);
+legend(h,'QSSPC','Arithmetic mean','Harmonic mean');
+set(gca,'FontSize',20);
+set(gca,'LineWidth',2);
+
