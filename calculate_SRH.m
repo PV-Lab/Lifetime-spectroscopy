7059933923ebe086686b1cda541e171a00ad32d1
maljensen <jensenma@mit.edu>
2016-02-29 11:11:53 -0500

    adding inputs to calculate_SRH function for more modularity and saving to specific location

--- calculate_SRH.m
+++ calculate_SRH.m
@@ -1,4 +1,4 @@
-function [tau_SRH,deltanq] = calculate_SRH(measured_file,SRV_file,deltanq,N_dop,T,type,W,D)
+function [tau_SRH,deltanq] = calculate_SRH(measured_file,SRV_file,deltanq,N_dop,T,type,W,D,saveFile,saveStart,sample_no)
 %[tau_SRH,deltan1] =
 %calculate_SRH(measured_file,SRV_file,deltanq,N_dop,T,type,W,D). Measured
 %file and SRV file should be of the format: 'filename.mat.' deltanq is
@@ -21,35 +21,31 @@ for i = 1:length(deltanq)
     tau_intr(i,1) = Richter(T,deltanq(i),N_dop,type);
 end
 
-load(measured_file); 
-[m,n] = size(dataSave); 
+data = load(measured_file); 
+deltan = data.deltanq;
+tau = data.tau_mean; 
+tau = tau';
 
-for i = 1:m
+tauq = interp1(deltan,tau,deltanq);
 
-    data = dataSave{i}; 
-    deltan = data(:,1); 
-    tau = data(:,2); 
+h=figure;
+loglog(deltanq,tauq.*1e6);
+hold all; 
+loglog(deltanq,tau_surf.*1e6); 
+hold all; 
+loglog(deltanq,tau_intr.*1e6); 
 
-    %Interpolate the lifetime data to use with the SRV later
-    tauq = interp1(deltan,tau,deltanq); 
+xlabel('Excess carrier density (cm^-^3)','FontSize',30);
+ylabel('Lifetime (\mus)','FontSize',30);
+axis([5e13 1e17 0 25000]);
 
-    figure;
-    loglog(deltanq,tauq.*1e6);
-    hold all; 
-    loglog(deltanq,tau_surf.*1e6); 
-    hold all; 
-    loglog(deltanq,tau_intr.*1e6); 
+tau_SRH = ((1./tauq)-(1./tau_intr')-(1./tau_surf)).^(-1);
+hold all;
+loglog(deltanq,tau_SRH.*1e6);
+legend('Measured','Surface','Intrinsic','SRH');
+title(['Sample ' sample_no],'FontSize',30); 
 
-    xlabel('Excess carrier density (cm^-^3)','FontSize',30);
-    ylabel('Lifetime (\mus)','FontSize',30);
-    axis([5e13 1e17 0 25000]);
+hgsave(h,[saveStart 'LifetimeBreakdown']);
+print(h,'-dpng','-r0',[saveStart 'LifetimeBreakdown.png']); 
+save(saveFile,'tau_SRH','tau_intr','tauq','tau_surf','deltanq');
 
-    tau_SRH_hold = ((1./tau_meas)-(1./tau_intr)-(1./tau_surf)).^(-1);
-    hold all;
-    loglog(deltanq,tau_SRH_hold.*1e6);
-    legend('Measured','Surface','Intrinsic','SRH');
-    title(['Sample ' num2str(i)],'FontSize',30); 
-    
-    tau_SRH(:,i) = tau_SRH_hold; 
-    
-end
