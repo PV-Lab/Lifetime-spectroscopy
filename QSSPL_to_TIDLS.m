bbd7cea7ee2eb7bf7d7d814a6b840c4dd84b382c
maljensen <jensenma@mit.edu>
2016-09-08 22:43:35 +1000

    writing script to format QSSPC for TIDLS processing

--- QSSPL_to_TIDLS.m
+++ QSSPL_to_TIDLS.m
@@ -7,26 +7,6 @@ filenames = {[dirname '\A23-6_25C.txt'];...
     [dirname '\A23-6_100C.txt'];...
     [dirname '\A23-6_150C.txt'];...
     [dirname '\A23-6_200C.txt']};
-%Sample parameters
-% temperature = {};
-% thickness_samp = ;
-% doping_samp = ;
-% type_samp = 'p'; 
-% OC = {0.7;0.7;0.7;0.7};
-% FS_samp = ;
-% 
-% %First process into the expected input for TIDLS measurements
-% %The inputs we need are thickness, doping, type, optical constant,
-% %temperature, FS used.
-% %type, FS, thickness, doping are all the same regardless of T
-% type = cell(size(filenames)); FS = cell(size(filenames)); 
-% thickness = cell(size(filenames)); doping = cell(size(filenames));
-% for i = 1:length(filenames)
-%     type{i} = type_samp;
-%     FS{i} = FS_samp;
-%     thickness{i} = thickness_samp;
-%     doping{i} = doping_samp;
-% end
 
 %!!! This is a placeholder
 sample_param_filename = 'C:\Users\Mallory\Dropbox (MIT)\TIDLS at UNSW\Advanced system measurements\20160804\For processing\python_inputs.mat';
@@ -38,42 +18,14 @@ for i = 1:length(filenames)
     [tau_mean,deltanq]=average_tau([dirname '\Raw_data.mat'],dirname);
 end
 
-%% As an alternative, try averaging the raw data. But first we need to figure out how to correct the raw data
-clear all; close all; 
-dirname = 'C:\Users\Mallory\Dropbox (MIT)\TIDLS at UNSW\Advanced system measurements\20160811\A23-6';
-file = [dirname '\A23-6_25C_1.Raw Data.dat'];
-savename = [dirname '\test.mat'];
-[data_now,time,PC,PL,gen]=format_QSSPL_rawdata(file,savename);
-
-%Define the wait time before the pulse arrives
-time_before = .001; %s
-[PC_corr] = correct_dark_voltage(time,PC,time_before);
-% %Let's take 75% of that just to be conservative
-% time_before = time_before*0.75; 
-% %Now look for the points to find the dark voltage on PC
-% PC_dark = PC(find(time<=time_before)); 
-% PC_dark = nanmean(PC_dark); 
-% %Now correct all values for the dark voltage
-% PC = PC-PC_dark; 
-% %Now replot the results
-% figure;
-% plot(time,gen);
-% hold all;
-% plot(time,PC);
-% hold all;
-% plot(time,PL);
-% xlabel('Time [s]','FontSize',20); 
-% ylabel('Voltage [V]','FontSize',20);
-% legend('Generation','PC','PL'); 
-% title('Dark voltage corrected','FontSize',20); 
 %% Now try averaging for a series 
 clear all; close all; 
-dirname = 'C:\Users\Mallory\Dropbox (MIT)\TIDLS at UNSW\Advanced system measurements\20160815\IR 1-64';
-sample = '16-6-28-P-2'; 
-Ctonum = 'C_1-64_wIR_';
+dirname = 'C:\Users\Mallory\Dropbox (MIT)\TIDLS at UNSW\Advanced system measurements\20160902';
+sample = '94-20-3'; 
+Ctonum = 'C_glassND_1-1_';
 time_before = .001; %s
-T = [25]; 
-num_repeats = [10]; 
+T = [-75 -125]; 
+num_repeats = [10 3]; 
 [dataSave] = average_QSSPL(dirname,sample,T,num_repeats,time_before,Ctonum);
 
 %Now, given this data, we want to write this to a new file which can be
@@ -83,60 +35,122 @@ for i = 1:length(T)
     write_QSSPL_analyser(dirname,sample,T(i),matrix_to_write,Ctonum);
 end
     
-% i = 1;
-%     %Make the filename to write to
-%     filename_write = [dirname '\' sample '_' num2str(T(i)) 'C_averaged.Raw Data.dat'];
-% %     fileID = fopen(filename_write,'w'); 
-%     %We need to write the first line
-%     first_line = '# Time (s)	Generation (V)	PC (V)	PL (V)';
-%     dlmwrite(filename_write,first_line,'delimiter',''); 
-%     %Now append the data
-%     load([dirname '\A23-6_200C_3_rawData.mat']);
-%     matrix_to_write = [time,gen,PC,PL];
-%     dlmwrite(filename_write,matrix_to_write,'-append','delimiter','\t','precision','%-6.18e','newline','pc'); 
-%     %Make the .inf file
-%     copyfile([dirname '\' sample '_' num2str(T(i)) 'C_1.inf'],[dirname '\' sample '_' num2str(T(i)) 'C_averaged.inf']);
+%% Now we prepare the data in our expected TIDLS format
+%Tell us how the files were entered in terms of temperature. These are the
+%actual sample temperatures in K. 
+direction = 'ascending'; 
+sample_name = '22-102-8'; 
+T = [466 418 370 298]; 
+dirname = 'C:\Users\Mallory\Dropbox (MIT)\TIDLS at UNSW\Advanced system measurements\By sample\22-102-8\flash only';
 
-% for i = 1:length(T)
-%     for j =1:num_repeats(i)
-%         %Make filename
-%         filename = [dirname '\' sample '_' num2str(T(i)) 'C_' num2str(j)...
-%             '.Raw Data.dat'];
-%         %Make the savename
-%         savename = [dirname '\' sample '_' num2str(T(i)) 'C_' num2str(j)...
-%             '_rawData.mat'];
-%         %Read the data
-%         [data_now,time,PC,PL,gen]=format_QSSPL_rawdata(filename,savename);
-%         %Correct the PC
-%         [PC_corr] = correct_dark_voltage(time,PC,time_before);
-%         %Let's assume the time vector is the same for all of the
-%         %measurements
-%         if j == 1
-%             PC_sum = PC_corr; 
-%             gen_sum = gen; 
-%             time_store = time; 
-%         else
-%             PC_sum = PC_sum+PC_corr; 
-%             gen_sum = gen_sum+gen; 
-%         end
-%         if isequal(time,time_store)==0
-%             disp('Error with time vectors, times are not equivalent sample-to-sample.'); 
-%         else
-%             time_store = time; 
-%         end
-%     end
-%     %Now we have the average for our given temperature
-%     PC_overall = PC_sum./num_repeats(i);
-%     gen_overall = gen_sum./num_repeats(i); 
-%     %Plot the result
-%     figure;
-%     plot(time_store,gen_overall);
-%     hold all;
-%     plot(time_store,PC_overall);
-%     xlabel('Time [s]','FontSize',20); 
-%     ylabel('Voltage [V]','FontSize',20);
-%     legend('Generation','PC'); 
-%     title(['Average PC, T = ' num2str(T(i)) 'C'],'FontSize',20); 
+%Read and save the raw data
+data_filename = [dirname '\' sample_name '_all_' direction '.txt']; 
+format_for_TIDLS(data_filename,dirname,'PL');
+
+%Now that we've saved the raw data, we need to save the sample parameters
+%as expected. For this we read the .inf files
+% if direction == 'ascending'
+%     T_list = flip(T); 
+% else
+%     T_list = T; 
 % end
-%             
-        
+% for i = 1:length(T)
+%     %Make the .inf filename
+%     inf_name = [dirname '\' sample_name '_' num2str(T) 'C_averaged.inf']; 
+%     %Read the inf
+
+%For the moment, just manually enter the information
+for i = 1:length(T)
+    temperature{i} = T(i)-273.15; 
+    resistivity{i} = 1000; 
+    thickness{i} = .0173; 
+    OC{i} = 0.8; 
+    calibration_factor{i} = 1; 
+    doping{i} = 8.5e15; 
+    fileListShort{i} = [sample_name '_' num2str(T(i)) 'C'];
+end
+info = struct('filename',fileListShort,'thickness',thickness,'resistivity',...
+    resistivity,'measured_resistivity',resistivity,'optical_constant',OC,...
+    'calibration_factor',calibration_factor,'temperature',temperature,'doping',doping);
+save([dirname '\meas_info.mat'],'info'); 
+
+%% Stitch together data from multiple measurements
+clear all; close all; 
+filename = 'C:\Users\Mallory\Dropbox (MIT)\TIDLS at UNSW\Advanced system measurements\By sample\22-25-8\25C\Attempt 2\22-25-8.txt'; 
+dirname = 'C:\Users\Mallory\Dropbox (MIT)\TIDLS at UNSW\Advanced system measurements\By sample\22-25-8\25C\Attempt 2';
+% format_for_TIDLS(filename,dirname,'PC');
+load([dirname '\PC\Raw_data.mat']); 
+dataPC = dataSave; 
+load([dirname '\PL\Raw_data.mat']);
+dataPL = dataSave; 
+figure;
+for i = 1:length(dataPC)
+    to_plot = dataPC{i}; 
+    loglog(to_plot(:,1),to_plot(:,2),'-','LineWidth',2); 
+    hold all; 
+    to_plot = dataPL{i}; 
+    loglog(to_plot(:,1),to_plot(:,2),'--','LineWidth',2); 
+    hold all; 
+end
+xlabel('Excess carrier density [cm^-^3]','FontSize',20);
+ylabel('Lifetime [s]','FontSize',20); 
+%Now try to combine all the data. Just try a simple combination
+for i = 1:length(dataPC)
+    if i == 1
+        togetherPC = dataPC{i}; 
+        togetherPL = dataPL{i}; 
+    else
+        togetherPC = [togetherPC;dataPC{i}];
+        togetherPL = [togetherPL;dataPL{i}];
+    end
+end
+togetherPC = sortrows(togetherPC,1); 
+togetherPL = sortrows(togetherPL,1); 
+hold all; 
+loglog(togetherPC(:,1),togetherPC(:,2),'k-','LineWidth',2); 
+hold all; 
+loglog(togetherPL(:,1),togetherPL(:,2),'k--','LineWidth',2); 
+
+%% 
+clear all; close all; 
+sample = '16-4-28-N'; 
+dirname = 'C:\Users\Mallory\Dropbox (MIT)\TIDLS at UNSW\Advanced system measurements\20160906\16-4-28-N'; 
+T = [-125 -75 -25 25 75]; %C
+dataStore = cell(length(T),1); 
+figure; 
+for i = 1:length(T)
+    filename = [dirname '\' num2str(T(i)) 'C\' sample '.txt']; 
+    format_for_TIDLS(filename,[dirname '\' num2str(T(i)) 'C'],'PC');
+    load([dirname '\' num2str(T(i)) 'C\Raw_data.mat']); 
+    data_now = dataSave; 
+    for j = 1:length(data_now)
+        if j == 1
+            togetherPC = data_now{j}; 
+        else
+            togetherPC = [togetherPC;data_now{j}];
+        end
+    end
+    togetherPC = sortrows(togetherPC,1); 
+    loglog(togetherPC(:,1),togetherPC(:,2),'k-','LineWidth',2); 
+    hold all; 
+    dataStore{i} = togetherPC; 
+end
+
+T = [148 198 248 297 342]; 
+for i = 1:length(T)
+    temperature{i} = T(i)-273.15; 
+    resistivity{i} = 1000; 
+    thickness{i} = .0270; 
+    OC{i} = 0.8; 
+    calibration_factor{i} = 1; 
+    doping{i} = 1.6e15; 
+    fileListShort{i} = [sample '_' num2str(T(i)) 'C'];
+end
+info = struct('filename',fileListShort,'thickness',thickness,'resistivity',...
+    resistivity,'measured_resistivity',resistivity,'optical_constant',OC,...
+    'calibration_factor',calibration_factor,'temperature',temperature,'doping',doping);
+save([dirname '\meas_info.mat'],'info'); 
+
+dataSave = dataStore; 
+save([dirname '\Raw_data.mat'],'dataSave'); 
+
