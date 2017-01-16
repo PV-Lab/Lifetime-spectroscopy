e622332ec25f7d6142ddb1c105d62c46d977a233
maljensen <jensenma@mit.edu>
2017-01-16 11:19:07 -0500

    Adding a section at the top to process raw data prior to analysis

--- analyze_HF_passivation.m
+++ analyze_HF_passivation.m
@@ -21,11 +21,38 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 %}
+%% First process the raw data
+clear all; close all; clc; 
+dirname = 'C:\Users\Mallory Jensen\Documents\LeTID\Hydrogenation experiment\HF passivation\January 13 2017'; 
+% samples = {'44a','45a','49a','50a','52a','53a','54a','55a','56a','60a','61a','C-1','C-2','H-1','H-2','FZ'};
+samples = {'H-2','FZ'};
+for index = 1:length(samples)
+    [fileList,fileListShort] = getAllFiles([dirname '\' samples{index}]); 
+    savename = [dirname '\' samples{index} '\Raw_data.mat']';
+    process_xls_data([dirname '\' samples{index}],savename);
+    %We also want to store all of the information for each file
+    %T, thickness, resistivity (entered/measured), type, optical constant, calibration,
+    %1/64 or 1/1
+    for file = 1:length(fileList)
+        this_file = fileList{file};
+        thick{file,1} = xlsread(this_file,'User','B6');
+        res{file,1} = xlsread(this_file,'User','C6');
+        oc{file,1} = xlsread(this_file,'User','E6');
+        temp{file,1} = 25;
+        meas_res{file,1} = xlsread(this_file,'Summary','M2');%'Q2');
+        calib{file,1} = xlsread(this_file,'Summary','S2');%'T2');
+        doping{file,1} = xlsread(this_file,'Summary','E2');
+    end
+    info = struct('filename',fileListShort,'thickness',thick,'resistivity',res,'measured_resistivity',meas_res,'optical_constant',oc,'calibration_factor',calib,'temperature',temp,'doping',doping);
+    save([dirname '\' samples{index} '\meas_info.mat'],'info');
+end
+
+%% Now analyze the data
 clear all; close all; clc;
 %Process data after HF passivation
 
-dirname = 'C:\Users\Mallory Jensen\Documents\LeTID\Hydrogenation experiment\HF passivation\January 9 2017'; 
-samples = {'44a','45a','49a','50a','52a','53a','54a','55a','56a','60a','61a','C-1','FZ','H-1','Hpass-1'};
+dirname = 'C:\Users\Mallory Jensen\Documents\LeTID\Hydrogenation experiment\HF passivation\January 13 2017'; 
+samples = {'44a','45a','49a','50a','52a','53a','54a','55a','56a','60a','61a','C-1','C-2','H-1','H-2','FZ'};
 lifetime_store = zeros(length(samples),1); 
 
 for i = 1:length(samples)
