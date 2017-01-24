b5d437aa7ff692f15f3c418e33e383f2b2056abd
maljensen <jensenma@mit.edu>
2017-01-24 16:39:31 -0500

    adding section to plot degradation curves

--- analyze_HF_passivation.m
+++ analyze_HF_passivation.m
@@ -23,7 +23,7 @@ SOFTWARE.
 %}
 %% First process the raw data
 clear all; close all; clc; 
-dirname = 'C:\Users\Mallory Jensen\Documents\LeTID\Hydrogenation experiment\HF passivation\January 19 2017'; 
+dirname = 'C:\Users\Mallory Jensen\Documents\LeTID\Hydrogenation experiment\HF passivation\January 23 2017'; 
 % samples = {'44a','45a','49a','50a','52a','53a','54a','55a','56a','60a','61a','C-1','C-2','H-1','H-2','FZ'};
 samples = {'44a','45a','49a','50a','52a','53a','54a','55a','56a','60a','61a','H-1','H-2','FZ','FZ-12'};
 for index = 1:length(samples)
@@ -51,7 +51,7 @@ end
 clear all; close all; clc;
 %Process data after HF passivation
 
-dirname = 'C:\Users\Mallory Jensen\Documents\LeTID\Hydrogenation experiment\HF passivation\January 19 2017'; 
+dirname = 'C:\Users\Mallory Jensen\Documents\LeTID\Hydrogenation experiment\HF passivation\January 23 2017'; 
 % samples = {'44a','45a','49a','50a','52a','53a','54a','55a','56a','60a','61a','C-1','C-2','H-1','H-2','FZ'};
 samples = {'44a','45a','49a','50a','52a','53a','54a','55a','56a','60a','61a','H-1','H-2','FZ','FZ-12'};
 lifetime_store = zeros(length(samples),1); 
@@ -99,10 +99,11 @@ dirname1 = 'C:\Users\Mallory Jensen\Documents\LeTID\Hydrogenation experiment\HF 
 dirname2 = 'C:\Users\Mallory Jensen\Documents\LeTID\Hydrogenation experiment\HF passivation\January 13 2017'; 
 dirname3 = 'C:\Users\Mallory Jensen\Documents\LeTID\Hydrogenation experiment\HF passivation\January 17 2017'; 
 dirname4 = 'C:\Users\Mallory Jensen\Documents\LeTID\Hydrogenation experiment\HF passivation\January 19 2017';
-dirnames = {dirname1 dirname2 dirname3 dirname4}; 
+dirname5 = 'C:\Users\Mallory Jensen\Documents\LeTID\Hydrogenation experiment\HF passivation\January 23 2017';
+dirnames = {dirname1 dirname2 dirname3 dirname4 dirname5}; 
 % samples = {'44a','45a','49a','50a','52a','53a','54a','55a','56a','60a','61a','C-1','C-2','H-1','H-2','FZ'};
 samples = {'44a','45a','49a','50a','52a','53a','54a','55a','56a','60a','61a','H-1','H-2','FZ','FZ-12'};
-savename = '_2000s_lifetime summary';
+savename = '_3000s_lifetime summary';
 for i = 1:length(samples)
     h=figure('units','normalized','outerposition',[0 0 1 1]);
     curves = [];
@@ -120,7 +121,13 @@ for i = 1:length(samples)
             flag = 0; 
         end
         if flag == 1
-            for j = 1:length(dataSave)
+%             for j = 1:length(dataSave)
+            if length(dataSave)>1
+                t = 2; 
+            else
+                t = 1;
+            end
+            for j  = t %just the second measurement in each set
                 datanow = dataSave{j}; 
                 curves(count)=loglog(datanow(:,1),datanow(:,2),'LineWidth',2); 
                 hold all; 
@@ -142,3 +149,163 @@ for i = 1:length(samples)
     hgsave(h,[savedirname '\' samples{i} savename]);
     print(h,'-dpng','-r0',[savedirname '\' samples{i} savename '.png']);
 end
+
+%% Make the degradation curves
+clear all; close all; clc; 
+savedirname = 'C:\Users\Mallory Jensen\Documents\LeTID\Hydrogenation experiment\HF passivation';
+savename = '_3000s_degradation';
+meas_details = 'C:\Users\Mallory Jensen\Documents\LeTID\Hydrogenation experiment\measurement_details.xlsx'; 
+deltan_target = 5e14; %target injection level for the measurements
+%Get the measurement details
+[meas,samples] = xlsread(meas_details,'measurements');
+samples(1,:) = []; 
+[times,filenames] = xlsread(meas_details,'filenames'); 
+%Make these the same size
+filenames = filenames(2:end,2); 
+
+lifetime_all = cell(length(samples),1); 
+norm_lifetime_all = cell(length(samples),1); 
+
+for i = 1:length(samples)
+    meas_thissample = meas(i,:);
+    lifetime_store = [];
+    for j = 1:length(meas_thissample)
+        if isnan(meas_thissample(j))==0
+            %now create the proper filename
+            findex = find(meas_thissample(j)==times);  
+            if meas_thissample(j)==0 && length(find(meas_thissample==0))==1
+                findex=2; %the second round of initial measurements
+            elseif length(find(meas_thissample==0))>1 && meas_thissample(j)==0
+                findex=findex(j);
+            end
+            filename = [filenames{findex} '\' samples{i} '\Raw_data.mat'];
+            load(filename);
+            if length(dataSave)>1
+                t = 2; 
+            else
+                t = 1;
+            end
+            datanow = dataSave{t}; 
+            [deltan,tau] = remove_duplicates(datanow(:,1),datanow(:,2));
+            %There's a little bug in this program, for now just do this... 
+            try
+                lifetime_store(j,1) = interp1(deltan,tau,deltan_target); 
+            catch
+                [deltan,tau] = remove_duplicates(deltan,tau);
+                lifetime_store(j,1) = interp1(deltan,tau,deltan_target);
+            end
+        end
+    end
+    nan_indices = find(isnan(meas_thissample)==1); 
+    meas_thissample(nan_indices) = []; 
+    lifetime_all{i} = [meas_thissample' lifetime_store]; 
+    norm_lifetime_all{i} = [meas_thissample' lifetime_store./lifetime_store(1)];
+end
+
+%Now, which samples do we want to plot together?
+control = {'H-1','H-2','FZ','FZ-12';...
+    'Unfired Cz (120 min H)','Fired Cz (120 min H)','FZ passivation','FZ degradation'};
+fired = {'49a','53a','56a','52a','55a','60a';...
+    '0 min','10 min','30 min','120 min','30 min no H','LeTID control'};
+unfired = {'61a','54a','50a','45a','45a','60a';...
+    '0 min','10 min','30 min','120 min','30 min no H','LeTID control'};
+
+lifetime_raw=figure('units','normalized','outerposition',[0 0 1 1]);
+lifetime_norm=figure('units','normalized','outerposition',[0 0 1 1]);
+[nothing,samp] = size(control); 
+labels = {}; 
+for i = 1:samp
+    index = find(strcmp(control{1,i},samples)==1);
+    raw_now = lifetime_all{index}; 
+    norm_now = norm_lifetime_all{index}; 
+    figure(lifetime_raw); 
+    plot(raw_now(:,1),raw_now(:,2),'-o','LineWidth',3,'MarkerSize',10); 
+    hold all; 
+    figure(lifetime_norm); 
+    plot(norm_now(:,1),norm_now(:,2),'-o','LineWidth',3,'MarkerSize',10);
+    hold all; 
+    labels{i,1} = control{2,i}; 
+end
+figure(lifetime_raw); 
+xlabel('time [s]','FontSize',25); 
+ylabel('lifetime [s]','FontSize',25); 
+legend(labels); 
+title('control samples','FontSize',25); 
+set(0,'defaultAxesFontSize', 20)
+hgsave(lifetime_raw,[savedirname '\controls' savename]);
+print(lifetime_raw,'-dpng','-r0',[savedirname '\controls' savename '.png']);
+figure(lifetime_norm); 
+xlabel('time [s]','FontSize',25); 
+ylabel('norm. lifetime [-]','FontSize',25); 
+legend(labels); 
+title('control samples','FontSize',25); 
+set(0,'defaultAxesFontSize', 20)
+hgsave(lifetime_norm,[savedirname '\controls_norm' savename]);
+print(lifetime_norm,'-dpng','-r0',[savedirname '\controls_norm' savename '.png']);
+    
+lifetime_raw=figure('units','normalized','outerposition',[0 0 1 1]);
+lifetime_norm=figure('units','normalized','outerposition',[0 0 1 1]);
+[nothing,samp] = size(unfired); 
+labels = {};
+for i = 1:samp
+    index = find(strcmp(unfired{1,i},samples)==1);
+    raw_now = lifetime_all{index}; 
+    norm_now = norm_lifetime_all{index}; 
+    figure(lifetime_raw); 
+    plot(raw_now(:,1),raw_now(:,2),'-o','LineWidth',3,'MarkerSize',10); 
+    hold all; 
+    figure(lifetime_norm); 
+    plot(norm_now(:,1),norm_now(:,2),'-o','LineWidth',3,'MarkerSize',10);
+    hold all; 
+    labels{i,1} = unfired{2,i}; 
+end
+figure(lifetime_raw); 
+xlabel('time [s]','FontSize',25); 
+ylabel('lifetime [s]','FontSize',25); 
+legend(labels); 
+title('unfired samples','FontSize',25); 
+set(0,'defaultAxesFontSize', 20)
+hgsave(lifetime_raw,[savedirname '\unfired' savename]);
+print(lifetime_raw,'-dpng','-r0',[savedirname '\unfired' savename '.png']);
+figure(lifetime_norm); 
+xlabel('time [s]','FontSize',25); 
+ylabel('norm. lifetime [-]','FontSize',25); 
+legend(labels); 
+title('unfired samples','FontSize',25); 
+set(0,'defaultAxesFontSize', 20)
+hgsave(lifetime_norm,[savedirname '\unfired_norm' savename]);
+print(lifetime_norm,'-dpng','-r0',[savedirname '\unfired_norm' savename '.png']);
+
+lifetime_raw=figure('units','normalized','outerposition',[0 0 1 1]);
+lifetime_norm=figure('units','normalized','outerposition',[0 0 1 1]);
+[nothing,samp] = size(fired); 
+labels = {};
+for i = 1:samp
+    index = find(strcmp(fired{1,i},samples)==1);
+    raw_now = lifetime_all{index}; 
+    norm_now = norm_lifetime_all{index}; 
+    figure(lifetime_raw); 
+    plot(raw_now(:,1),raw_now(:,2),'-o','LineWidth',3,'MarkerSize',10); 
+    hold all; 
+    figure(lifetime_norm); 
+    plot(norm_now(:,1),norm_now(:,2),'-o','LineWidth',3,'MarkerSize',10);
+    hold all; 
+    labels{i,1} = fired{2,i}; 
+end
+figure(lifetime_raw); 
+xlabel('time [s]','FontSize',25); 
+ylabel('lifetime [s]','FontSize',25); 
+legend(labels); 
+title('fired samples','FontSize',25); 
+set(0,'defaultAxesFontSize', 20)
+hgsave(lifetime_raw,[savedirname '\fired' savename]);
+print(lifetime_raw,'-dpng','-r0',[savedirname '\fired' savename '.png']);
+figure(lifetime_norm); 
+xlabel('time [s]','FontSize',25); 
+ylabel('norm. lifetime [-]','FontSize',25); 
+legend(labels); 
+title('fired samples','FontSize',25); 
+set(0,'defaultAxesFontSize', 20)
+hgsave(lifetime_norm,[savedirname '\fired_norm' savename]);
+print(lifetime_norm,'-dpng','-r0',[savedirname '\fired_norm' savename '.png']);
+
