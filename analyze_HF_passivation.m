cf1c5ccbf08b73c7cde0a03d58c00edf7cdfa781
maljensen <jensenma@mit.edu>
2017-04-25 17:11:35 -0400

    making edits to accomodate different experiment

--- analyze_HF_passivation.m
+++ analyze_HF_passivation.m
@@ -23,12 +23,8 @@ SOFTWARE.
 %}
 %% First process the raw data
 clear all; close all; clc; 
-dirname = 'C:\Users\Mallory Jensen\Documents\LeTID\Hydrogenation experiment\HF passivation\April 21 2017'; 
-% samples = {'44a','45a','49a','50a','52a','53a','54a','55a','56a','60a','61a','C-1','C-2','H-1','H-2','FZ'};
-samples = {'44a','45a','49a','50a','52a','53a','54a','55a','56a','60a','61a','H-1','H-2','FZ','FZ-12','68-2'};
-% samples = {'56a','60a','61a','H-1','H-2','FZ','FZ-12','68-2'};
-% samples = {'FZ-12','68-2'};
-% samples = {'H-1','H-2','FZ','FZ-12','68-2'};
+dirname = 'C:\Users\Mallory Jensen\Documents\LeTID\Dartboard\Degradation\1700s'; 
+samples = {'Ti-h-9','Ni-h-9','Mo-h-9','V-h-9','C-h-9'};
 for index = 1:length(samples)
     [fileList,fileListShort] = getAllFiles([dirname '\' samples{index}]); 
     savename = [dirname '\' samples{index} '\Raw_data.mat']';
@@ -54,9 +50,8 @@ end
 clear all; close all; clc;
 %Process data after HF passivation
 
-dirname = 'C:\Users\Mallory Jensen\Documents\LeTID\Hydrogenation experiment\HF passivation\April 21 2017'; 
-% samples = {'44a','45a','49a','50a','52a','53a','54a','55a','56a','60a','61a','C-1','C-2','H-1','H-2','FZ'};
-samples = {'44a','45a','49a','50a','52a','53a','54a','55a','56a','60a','61a','H-1','H-2','FZ','FZ-12','68-2'};
+dirname = 'C:\Users\Mallory Jensen\Documents\LeTID\Dartboard\Degradation\1700s'; 
+samples = {'Ti-h-9','Ni-h-9','Mo-h-9','V-h-9','C-h-9'};
 lifetime_store = zeros(length(samples),1); 
 
 for i = 1:length(samples)
@@ -103,48 +98,26 @@ end
 
 %% Analyze different states together
 clear all; close all; clc;
-savedirname = 'C:\Users\Mallory Jensen\Documents\LeTID\Hydrogenation experiment\HF passivation';
-dirname1 = 'C:\Users\Mallory Jensen\Documents\LeTID\Hydrogenation experiment\HF passivation\January 9 2017';
-dirname2 = 'C:\Users\Mallory Jensen\Documents\LeTID\Hydrogenation experiment\HF passivation\January 13 2017'; 
-dirname3 = 'C:\Users\Mallory Jensen\Documents\LeTID\Hydrogenation experiment\HF passivation\January 17 2017'; 
-dirname4 = 'C:\Users\Mallory Jensen\Documents\LeTID\Hydrogenation experiment\HF passivation\January 19 2017';
-dirname5 = 'C:\Users\Mallory Jensen\Documents\LeTID\Hydrogenation experiment\HF passivation\January 23 2017';
-dirname6 = 'C:\Users\Mallory Jensen\Documents\LeTID\Hydrogenation experiment\HF passivation\January 25 2017';
-dirname7 = 'C:\Users\Mallory Jensen\Documents\LeTID\Hydrogenation experiment\HF passivation\January 27 2017';
-dirname8 = 'C:\Users\Mallory Jensen\Documents\LeTID\Hydrogenation experiment\HF passivation\January 31 2017';
-dirname9 = 'C:\Users\Mallory Jensen\Documents\LeTID\Hydrogenation experiment\HF passivation\February 2 2017';
-dirname10 = 'C:\Users\Mallory Jensen\Documents\LeTID\Hydrogenation experiment\HF passivation\February 6 2017';
-dirname11 = 'C:\Users\Mallory Jensen\Documents\LeTID\Hydrogenation experiment\HF passivation\February 8 2017';
-dirname12 = 'C:\Users\Mallory Jensen\Documents\LeTID\Hydrogenation experiment\HF passivation\February 13 2017';
-dirname13 = 'C:\Users\Mallory Jensen\Documents\LeTID\Hydrogenation experiment\HF passivation\February 15 2017';
-dirname14 = 'C:\Users\Mallory Jensen\Documents\LeTID\Hydrogenation experiment\HF passivation\February 17 2017';
-% dirname15 = 'C:\Users\Mallory Jensen\Documents\LeTID\Hydrogenation experiment\HF passivation\March 1 2017'; 
-dirname15 = 'C:\Users\Mallory Jensen\Documents\LeTID\Hydrogenation experiment\HF passivation\March 1 2017\revised calibration';
-dirname16 = 'C:\Users\Mallory Jensen\Documents\LeTID\Hydrogenation experiment\HF passivation\March 8 2017';
-dirname17 = 'C:\Users\Mallory Jensen\Documents\LeTID\Hydrogenation experiment\HF passivation\March 11 2017';
-dirname18 = 'C:\Users\Mallory Jensen\Documents\LeTID\Hydrogenation experiment\HF passivation\March 15 2017';
-% dirname19 = 'C:\Users\Mallory Jensen\Documents\LeTID\Hydrogenation experiment\HF passivation\March 17 2017';
-dirname19 = 'C:\Users\Mallory Jensen\Documents\LeTID\Hydrogenation experiment\HF passivation\March 21 2017';
-dirname20 = 'C:\Users\Mallory Jensen\Documents\LeTID\Hydrogenation experiment\HF passivation\March 23 2017';
-dirname21 = 'C:\Users\Mallory Jensen\Documents\LeTID\Hydrogenation experiment\HF passivation\March 30 2017';
-dirname22 = 'C:\Users\Mallory Jensen\Documents\LeTID\Hydrogenation experiment\HF passivation\April 3 2017';
-dirname23 = 'C:\Users\Mallory Jensen\Documents\LeTID\Hydrogenation experiment\HF passivation\April 11 2017';
-dirname24 = 'C:\Users\Mallory Jensen\Documents\LeTID\Hydrogenation experiment\HF passivation\April 13 2017';
-dirname25 = 'C:\Users\Mallory Jensen\Documents\LeTID\Hydrogenation experiment\HF passivation\April 20 2017';
-dirname26 = 'C:\Users\Mallory Jensen\Documents\LeTID\Hydrogenation experiment\HF passivation\April 21 2017';
-% dirnames = {dirname1 dirname2 dirname3 dirname4 dirname5 dirname6 dirname7 dirname8 dirname9 dirname10 dirname11 dirname12}; 
+savedirname = 'C:\Users\Mallory Jensen\Documents\LeTID\Dartboard\Degradation';
+dirname1 = 'C:\Users\Mallory Jensen\Documents\LeTID\Dartboard\Degradation\0s';
+dirname2 = 'C:\Users\Mallory Jensen\Documents\LeTID\Dartboard\Degradation\100s';
+dirname3 = 'C:\Users\Mallory Jensen\Documents\LeTID\Dartboard\Degradation\200s';
+dirname4 = 'C:\Users\Mallory Jensen\Documents\LeTID\Dartboard\Degradation\300s';
+dirname5 = 'C:\Users\Mallory Jensen\Documents\LeTID\Dartboard\Degradation\400s';
+dirname6 = 'C:\Users\Mallory Jensen\Documents\LeTID\Dartboard\Degradation\500s';
+dirname7 = 'C:\Users\Mallory Jensen\Documents\LeTID\Dartboard\Degradation\600s';
+dirname8 = 'C:\Users\Mallory Jensen\Documents\LeTID\Dartboard\Degradation\700s';
+dirname9 = 'C:\Users\Mallory Jensen\Documents\LeTID\Dartboard\Degradation\800s';
+dirname10 = 'C:\Users\Mallory Jensen\Documents\LeTID\Dartboard\Degradation\900s';
+dirname11 = 'C:\Users\Mallory Jensen\Documents\LeTID\Dartboard\Degradation\1000s';
+dirname12 = 'C:\Users\Mallory Jensen\Documents\LeTID\Dartboard\Degradation\1700s';
 dirnames = {dirname2 dirname3 dirname4 dirname5 dirname6 dirname7 dirname8 ...
-    dirname10 dirname11 dirname12 dirname13 dirname14 dirname15 dirname16 ...
-    dirname17 dirname18 dirname19 dirname20 dirname21 dirname22 dirname23 ...
-    dirname24 dirname25 dirname26}; 
-labels = {'initial','1000s','2000s','3000s','4000s','5000s','10000s',...
-    '20000s','30000s','40000s','50000s','60000s','70000s','80000s','90000s',...
-    '100000s','154495','206005','258325','300025','349990','402730s',...
-    '454750s','508360s'};
+    dirname10 dirname11 dirname12}; 
+labels = {'initial','100s','200s','300s','400s','500s','600s',...
+    '700s','800s','900s','1000s','1700s'};
 cm = colormap(hsv(length(dirnames))); 
-% samples = {'44a','45a','49a','50a','52a','53a','54a','55a','56a','60a','61a','C-1','C-2','H-1','H-2','FZ'};
-samples = {'44a','45a','49a','50a','52a','53a','54a','55a','56a','60a','61a','H-1','H-2','FZ','FZ-12','68-2'};
-savename = '_508360s_lifetime summary';
+samples = {'Ti-h-9','Ni-h-9','Mo-h-9','V-h-9','C-h-9'};
+savename = '_1700s_lifetime summary';
 for i = 1:length(samples)
     h=figure('units','normalized','outerposition',[0 0 1 1]);
     curves = [];
@@ -194,11 +167,11 @@ end
 
 %% Make the degradation curves
 clear all; close all; clc; 
-savedirname = 'C:\Users\Mallory Jensen\Documents\LeTID\Hydrogenation experiment\HF passivation';
-savename = '_508360s_degradation';
-max_time = 508360; 
-meas_details = 'C:\Users\Mallory Jensen\Documents\LeTID\Hydrogenation experiment\measurement_details_removingInitial.xlsx'; 
-deltan_target = 6e14; %target injection level for the measurements, changed to 6e14 on 2/13/17 from 5e14
+savedirname = 'C:\Users\Mallory Jensen\Documents\LeTID\Dartboard\Degradation';
+savename = '_1700s_degradation';
+max_time = 1710; 
+meas_details = 'C:\Users\Mallory Jensen\Documents\LeTID\Dartboard\Degradation\measurement_details_removingInitial.xlsx'; 
+deltan_target = 5e14; %target injection level for the measurements, changed to 6e14 on 2/13/17 from 5e14
 %Get the measurement details
 [meas,samples] = xlsread(meas_details,'measurements');
 samples(1,:) = []; 
@@ -253,37 +226,9 @@ for i = 1:length(samples)
     norm_lifetime_all{i} = [meas_thissample' lifetime_store./lifetime_store(1)];
 end
 
-%Now try to get the degradation curve, removing the surface component
-SRV_control = 'FZ';
-FZ_dop = 5.7e15;
-[De,Dh] = diffusivity(300,'p',FZ_dop,deltan_target); 
-D_FZ  = De; 
-W_FZ = .027; 
-index = find(strcmp(SRV_control,samples)==1);
-raw_now = lifetime_all{index};
-SRV = zeros(length(raw_now),1); 
-for i = 1:length(SRV)
-    tau_intr = Richter(300,deltan_target,FZ_dop,'p');
-    tau_surf = ((1./raw_now(i,2))-(1./tau_intr)).^(-1);
-    SRV(i) = W_FZ./((tau_surf-((1/D_FZ)*((W_FZ/pi)^2))).*2);
-end
-figure;
-plot(raw_now(:,1),SRV); 
-axis([0 max_time 0 10]);
-xlabel('time [s]','FontSize',25); 
-ylabel('SRV [cm/s]','FontSize',25); 
-SRV_t = raw_now(:,1); 
-
 %Now, which samples do we want to plot together?
-control = {'H-1','H-2','FZ','FZ-12','68-2';...
-    'Unfired Cz (120 min H)','Fired Cz (120 min H)','FZ passivation','FZ degradation','mc-Si undegraded'};
-control_param = [5e15 5e15 5e15 5e15 9e15; .025 .025 .025 .02 .017]; %first row doping, second row thickness 
-fired = {'49a','53a','56a','52a','55a','60a';...
-    '0 min','10 min','30 min','120 min','30 min no H','LeTID control'};
-fired_param = []; %first row doping, second row thickness
-unfired = {'61a','54a','50a','45a','44a','60a';...
-    '0 min','10 min','30 min','120 min','30 min no H','LeTID control'};
-unfired_param = []; %first row doping, second row thickness
+control = {'Ti-h-9','Ni-h-9','Mo-h-9','V-h-9','C-h-9';...
+    'Ti','Ni','Mo','V','Control'};
 
 lifetime_raw=figure('units','normalized','outerposition',[0 0 1 1]);
 lifetime_norm=figure('units','normalized','outerposition',[0 0 1 1]);
@@ -300,27 +245,13 @@ for i = 1:samp
     figure(lifetime_norm); 
     plot(norm_now(:,1),norm_now(:,2),'-o','LineWidth',3,'MarkerSize',10);
     hold all;  
-    %Get the lifetime, accounting for surface
-    doping_now = control_param(1,i); 
-    W_now = control_param(2,i); 
-    [D_now,Dh] = diffusivity(300,'p',doping_now,deltan_target);
-    tau_rev = zeros(length(raw_now),1); 
-    for j = 1:length(raw_now)
-        t_index = find(SRV_t==raw_now(j,1)); 
-        tau = raw_now(j,2); 
-        tau_surf = (W_now./(2.*SRV(t_index)))+((1/D_now).*((W_now/pi)^2)); %cm/s
-        tau_rev(j) = ((1./tau)-(1./tau_surf))^(-1); 
-    end
-    figure(lifetime_norm_corr); 
-    plot(norm_now(:,1),tau_rev./tau_rev(1),'-o','LineWidth',3,'MarkerSize',10);
-    hold all; 
     labels{i,1} = control{2,i};
 end
 figure(lifetime_raw); 
 xlabel('time [s]','FontSize',25); 
 ylabel('lifetime [s]','FontSize',25); 
 legend(labels); 
-title('control samples','FontSize',25); 
+title('high hydrogen','FontSize',25); 
 set(0,'defaultAxesFontSize', 20)
 hgsave(lifetime_raw,[savedirname '\controls' savename]);
 print(lifetime_raw,'-dpng','-r0',[savedirname '\controls' savename '.png']);
@@ -329,85 +260,7 @@ xlabel('time [s]','FontSize',25);
 ylabel('norm. lifetime [-]','FontSize',25); 
 legend(labels); 
 axis([0 max_time 0 2]);
-title('control samples','FontSize',25); 
+title('high hydrogen','FontSize',25); 
 set(0,'defaultAxesFontSize', 20)
 hgsave(lifetime_norm,[savedirname '\controls_norm' savename]);
 print(lifetime_norm,'-dpng','-r0',[savedirname '\controls_norm' savename '.png']);
-figure(lifetime_norm_corr); 
-xlabel('time [s]','FontSize',25); 
-ylabel('norm. lifetime (no surface) [-]','FontSize',25); 
-legend(labels); 
-axis([0 max_time 0 2]);
-title('control samples (surface removed)','FontSize',25); 
-set(0,'defaultAxesFontSize', 20)
-hgsave(lifetime_norm_corr,[savedirname '\controls_norm_nosurf' savename]);
-print(lifetime_norm_corr,'-dpng','-r0',[savedirname '\controls_norm_nosurf' savename '.png']);
-    
-lifetime_raw=figure('units','normalized','outerposition',[0 0 1 1]);
-lifetime_norm=figure('units','normalized','outerposition',[0 0 1 1]);
-[nothing,samp] = size(unfired); 
-labels = {};
-for i = 1:samp
-    index = find(strcmp(unfired{1,i},samples)==1);
-    raw_now = lifetime_all{index}; 
-    norm_now = norm_lifetime_all{index}; 
-    figure(lifetime_raw); 
-    plot(raw_now(:,1),raw_now(:,2),'-o','LineWidth',3,'MarkerSize',10); 
-    hold all; 
-    figure(lifetime_norm); 
-    plot(norm_now(:,1),norm_now(:,2),'-o','LineWidth',3,'MarkerSize',10);
-    hold all; 
-    labels{i,1} = unfired{2,i}; 
-end
-figure(lifetime_raw); 
-xlabel('time [s]','FontSize',25); 
-ylabel('lifetime [s]','FontSize',25); 
-legend(labels); 
-title('unfired samples','FontSize',25); 
-set(0,'defaultAxesFontSize', 20)
-hgsave(lifetime_raw,[savedirname '\unfired' savename]);
-print(lifetime_raw,'-dpng','-r0',[savedirname '\unfired' savename '.png']);
-figure(lifetime_norm); 
-xlabel('time [s]','FontSize',25); 
-ylabel('norm. lifetime [-]','FontSize',25); 
-legend(labels); 
-axis([0 max_time 0 2]);
-title('unfired samples','FontSize',25); 
-set(0,'defaultAxesFontSize', 20)
-hgsave(lifetime_norm,[savedirname '\unfired_norm' savename]);
-print(lifetime_norm,'-dpng','-r0',[savedirname '\unfired_norm' savename '.png']);
-
-lifetime_raw=figure('units','normalized','outerposition',[0 0 1 1]);
-lifetime_norm=figure('units','normalized','outerposition',[0 0 1 1]);
-[nothing,samp] = size(fired); 
-labels = {};
-for i = 1:samp
-    index = find(strcmp(fired{1,i},samples)==1);
-    raw_now = lifetime_all{index}; 
-    norm_now = norm_lifetime_all{index}; 
-    figure(lifetime_raw); 
-    plot(raw_now(:,1),raw_now(:,2),'-o','LineWidth',3,'MarkerSize',10); 
-    hold all; 
-    figure(lifetime_norm); 
-    plot(norm_now(:,1),norm_now(:,2),'-o','LineWidth',3,'MarkerSize',10);
-    hold all; 
-    labels{i,1} = fired{2,i}; 
-end
-figure(lifetime_raw); 
-xlabel('time [s]','FontSize',25); 
-ylabel('lifetime [s]','FontSize',25); 
-legend(labels); 
-title('fired samples','FontSize',25); 
-set(0,'defaultAxesFontSize', 20)
-hgsave(lifetime_raw,[savedirname '\fired' savename]);
-print(lifetime_raw,'-dpng','-r0',[savedirname '\fired' savename '.png']);
-figure(lifetime_norm); 
-axis([0 max_time 0 2]);
-xlabel('time [s]','FontSize',25); 
-ylabel('norm. lifetime [-]','FontSize',25); 
-legend(labels); 
-title('fired samples','FontSize',25); 
-set(0,'defaultAxesFontSize', 20)
-hgsave(lifetime_norm,[savedirname '\fired_norm' savename]);
-print(lifetime_norm,'-dpng','-r0',[savedirname '\fired_norm' savename '.png']);
-
