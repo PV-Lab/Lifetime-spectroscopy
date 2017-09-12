045bae91287d679331ef1ab3af563ee0c067ec60
maljensen <jensenma@mit.edu>
2017-09-12 17:03:31 -0400

    analyzing a new set of data, adding FZ-12 here

--- analyze_HF_passivation.m
+++ analyze_HF_passivation.m
@@ -24,7 +24,8 @@ SOFTWARE.
 %% First process the raw data
 clear all; close all; clc; 
 dirname = 'C:\Users\Mallory Jensen\Documents\LeTID\Hydrogenation experiment\HF passivation\August 30 2017'; 
-samples = {'44b','45b','49b','50b','52b','53b','54b','55b','56b','60b','61b','C-1','C-2','FZ','66-2'};
+% samples = {'44b','45b','49b','50b','52b','53b','54b','55b','56b','60b','61b','C-1','C-2','FZ','66-2','68-2','68-4','FZ-12','FZ-new'};
+samples = {'FZ-12'};
 for index = 1:length(samples)
     [fileList,fileListShort] = getAllFiles([dirname '\' samples{index}]); 
     savename = [dirname '\' samples{index} '\Raw_data.mat']';
@@ -58,7 +59,8 @@ clear all; close all; clc;
 %Process data after HF passivation
 
 dirname = 'C:\Users\Mallory Jensen\Documents\LeTID\Hydrogenation experiment\HF passivation\August 30 2017'; 
-samples = {'44b','45b','49b','50b','52b','53b','54b','55b','56b','60b','61b','C-1','C-2','FZ','66-2'};
+% samples = {'44b','45b','49b','50b','52b','53b','54b','55b','56b','60b','61b','C-1','C-2','FZ','66-2','68-2','68-4','FZ-12','FZ-new'};
+samples = {'FZ-12'};
 lifetime_store = zeros(length(samples),1); 
 
 for i = 1:length(samples)
@@ -111,11 +113,12 @@ dirname2 = 'C:\Users\Mallory Jensen\Documents\LeTID\Hydrogenation experiment\HF 
 dirname3 = 'C:\Users\Mallory Jensen\Documents\LeTID\Hydrogenation experiment\HF passivation\August 16 2017';
 dirname4 = 'C:\Users\Mallory Jensen\Documents\LeTID\Hydrogenation experiment\HF passivation\August 29 2017';
 dirname5 = 'C:\Users\Mallory Jensen\Documents\LeTID\Hydrogenation experiment\HF passivation\August 30 2017';
-samples = {'44b','45b','49b','50b','52b','53b','54b','55b','56b','60b','61b','C-1','C-2','66-2','FZ'};
-dirnames = {dirname1 dirname2 dirname3 dirname4 dirname5}; 
-labels = {'initial' '50s' '100s' '250s' '500s'};
+dirname6 = 'C:\Users\Mallory Jensen\Documents\LeTID\Hydrogenation experiment\HF passivation\September 8 2017';
+samples = {'44b','45b','49b','50b','52b','53b','54b','55b','56b','60b','61b','C-1','C-2','FZ','66-2','68-2','68-4','FZ-12','FZ-new'};
+dirnames = {dirname1 dirname2 dirname3 dirname4 dirname5 dirname6}; 
+labels = {'initial' '50s' '100s' '250s' '500s' '501s'};
 cm = colormap(hsv(length(dirnames)));
-savename = '_setb_500s_lifetime summary';
+savename = '_setb_501s_lifetime summary';
 for i = 1:length(samples)
     h=figure('units','normalized','outerposition',[0 0 1 1]);
     curves = [];
@@ -168,8 +171,8 @@ end
 %% Make the degradation curves
 clear all; close all; clc; 
 savedirname = 'C:\Users\Mallory Jensen\Documents\LeTID\Hydrogenation experiment\HF passivation';
-savename = '_500s_degradation';
-max_time = 500; 
+savename = '_501s_degradation';
+max_time = 501; 
 time_shift_E = 801610; %amount of time to shift company E measurements over for comparison after switch
 meas_details = 'C:\Users\Mallory Jensen\Documents\LeTID\Hydrogenation experiment\measurement_details_setb.xlsx'; 
 deltan_target = 6e14; %target injection level for the measurements, changed to 6e14 on 2/13/17 from 5e14
@@ -223,8 +226,8 @@ for i = 1:length(samples)
 end
 
 %Now, which samples do we want to plot together?
-control = {'C-1','C-2','FZ','66-2';...
-    'Unfired Cz (no H)','Fired Cz (no H)','FZ passivation','mc-Si 750C fired undegraded'};
+control = {'C-1','C-2','FZ','66-2','FZ-12';...
+    'Unfired Cz (no H)','Fired Cz (no H)','FZ passivation','mc-Si 750C fired undegraded','FZ degradation'};
 fired = {'50b','54b','44b','49b','61b','56b';...
     '0 min','10 min','30 min','120 min','30 min no H','LeTID control'};
 unfired = {'52b','45b','55b','60b','53b','56b';...
