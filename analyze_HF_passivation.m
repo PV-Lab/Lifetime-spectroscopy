afd09cc2b370acedfda5831de3939e9d7e967a5f
maljensen <jensenma@mit.edu>
2018-05-29 15:20:17 -0400

    processing latest data

--- analyze_HF_passivation.m
+++ analyze_HF_passivation.m
@@ -23,8 +23,9 @@ SOFTWARE.
 %}
 %% First process the raw data
 clear all; close all; clc; 
-dirname = 'C:\Users\Mallory Jensen\Documents\LeTID\PDG\round 3 data\05-21-2018_DegTime500855sec'; 
+dirname = 'C:\Users\Mallory Jensen\Documents\LeTID\PDG\round 3 data\05-28-2018_DegTime624438sec'; 
 samples = {'1-5','2-5','3-5','4-5','5-5','6-5','7-5','8-5','P-1'};
+% samples = {'P-1'};
 for index = 1:length(samples)
     [fileList,fileListShort] = getAllFiles([dirname '\' samples{index}]); 
     savename = [dirname '\' samples{index} '\Raw_data.mat']';
@@ -45,8 +46,8 @@ for index = 1:length(samples)
         res{file,1} = xlsread(this_file,'User','C6');
         oc{file,1} = xlsread(this_file,'User','E6');
         temp{file,1} = 25;
-        meas_res{file,1} = xlsread(this_file,'Summary','M2');%'Q2');
-        calib{file,1} = xlsread(this_file,'Summary','S2');%'T2');
+        meas_res{file,1} = xlsread(this_file,'Summary','N2');%'M2');%'Q2');
+        calib{file,1} = xlsread(this_file,'Summary','T2');%'S2');%'T2');
         doping{file,1} = xlsread(this_file,'Summary','E2');
     end
     info = struct('filename',fileListShort,'thickness',thick,'resistivity',res,'measured_resistivity',meas_res,'optical_constant',oc,'calibration_factor',calib,'temperature',temp,'doping',doping);
@@ -57,7 +58,7 @@ end
 clear all; close all; clc;
 %Process data after HF passivation
 
-dirname = 'C:\Users\Mallory Jensen\Documents\LeTID\PDG\round 3 data\05-21-2018_DegTime500855sec'; 
+dirname = 'C:\Users\Mallory Jensen\Documents\LeTID\PDG\round 3 data\05-28-2018_DegTime624438sec'; 
 samples = {'1-5','2-5','3-5','4-5','5-5','6-5','7-5','8-5','P-1'};
 lifetime_store = zeros(length(samples),1); 
 
@@ -128,7 +129,7 @@ clear all; close all; clc;
 % dirname7 = 'C:\Users\Mallory Jensen\Documents\LeTID\PDG\round 2 data\#3 degradation\January 31 2018';
 % dirnames = {dirname1 dirname2 dirname3 dirname4 dirname5 dirname6 dirname7}; 
 % labels = {'initial','2500s','5000s','10000s','25000s','50000s','101735s'};
-savedirname='C:\Users\Mallory Jensen\Documents\LeTID\PDG\round 3 data\summary\440350s';
+savedirname='C:\Users\Mallory Jensen\Documents\LeTID\PDG\round 3 data\summary\624438s';
 dirname1='C:\Users\Mallory Jensen\Documents\LeTID\PDG\round 3 data\03-26-2018_DegTime0sec';
 dirname2='C:\Users\Mallory Jensen\Documents\LeTID\PDG\round 3 data\03-27-2018_DegTime500sec';
 dirname3='C:\Users\Mallory Jensen\Documents\LeTID\PDG\round 3 data\03-28-2018_DegTime1000sec';
@@ -160,23 +161,27 @@ dirname28 = 'C:\Users\Mallory Jensen\Documents\LeTID\PDG\round 3 data\05-15-2018
 dirname29 = 'C:\Users\Mallory Jensen\Documents\LeTID\PDG\round 3 data\05-16-2018_DegTime440350sec';
 dirname30 = 'C:\Users\Mallory Jensen\Documents\LeTID\PDG\round 3 data\05-18-2018_DegTime468875sec';
 dirname31 = 'C:\Users\Mallory Jensen\Documents\LeTID\PDG\round 3 data\05-21-2018_DegTime500855sec';
+dirname32 = 'C:\Users\Mallory Jensen\Documents\LeTID\PDG\round 3 data\05-23-2018_DegTime531545sec';
+dirname33 = 'C:\Users\Mallory Jensen\Documents\LeTID\PDG\round 3 data\05-24-2018_DegTime559338sec';
+dirname34 = 'C:\Users\Mallory Jensen\Documents\LeTID\PDG\round 3 data\05-25-2018_DegTime598218sec';
+dirname35 = 'C:\Users\Mallory Jensen\Documents\LeTID\PDG\round 3 data\05-28-2018_DegTime624438sec';
 dirnames = {dirname1,dirname2,dirname3,dirname4,dirname5,dirname6,...
     dirname7,dirname8,dirname9,dirname10,dirname11,dirname12,dirname13,...
     dirname14,dirname15,dirname16,dirname17,dirname18,dirname19,dirname20,...
     dirname21,dirname22,dirname23,dirname24,dirname25,dirname26,dirname27,...
-    dirname29,dirname30,dirname31};
+    dirname29,dirname30,dirname31,dirname32,dirname33,dirname34,dirname35};
 labels= {'0s','500s','1000s','2500s','5000s','7500s','10000s','20000s',...
     '40000s','60000s','80000s','100000s','120000s','140000s','160000s',...
     '180000s','201300s','211240s','231240s','248080s','266510s','284880s',...
     '304880s','324880s','348810s','368810s','388810s','408810s','440350s',...
-    '468875','500855'};
+    '468875','500855','531545s','559338s','598218s','624438s'};
 cm = colormap(hsv(length(dirnames))); 
 % samples = {'1-6','2-6','3-6','4-6','5-6','6-6','7-6','8-6','P-1'};
 % samples = {'1-7','4-7','6-7','7-7'};
 % samples = {'2-7','3-7','5-7','8-7'};
 % samples = {'1-3','2-3','3-3','4-3','5-3','6-3','7-3','8-3','P-2'};
 samples = {'1-5','2-5','3-5','4-5','5-5','6-5','7-5','8-5','P-1'};
-savename = '_500855s_lifetime summary';
+savename = '_624438s_lifetime summary';
 for i = 1:length(samples)
     h=figure('units','normalized','outerposition',[0 0 1 1]);
     curves = [];
@@ -281,9 +286,9 @@ print(h,'-dpng','-r0',[savedirname '\initial lifetime summary with LTA.png']);
 
 %% Make the degradation curves
 clear all; close all; clc; 
-savedirname = 'C:\Users\Mallory Jensen\Documents\LeTID\PDG\round 3 data\summary\500855s';
-savename = '_500855s_degradation';
-max_time = 500855; 
+savedirname = 'C:\Users\Mallory Jensen\Documents\LeTID\PDG\round 3 data\summary\624438s';
+savename = '_624438s_degradation';
+max_time = 624438; 
 meas_details = 'C:\Users\Mallory Jensen\Documents\LeTID\PDG\round 3 data\measurement_details.xlsx'; 
 deltan_target = 6e14; %target injection level for the measurements, changed to 6e14 on 2/13/17 from 5e14
 %Get the measurement details
